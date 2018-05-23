8393910194dd19031a13e4ba27209b88a24c6576
ksrinivas788 <kishores4200@gmail.com>
2018-05-22 23:22:10 -0700

    instructions and other player points

--- RoommateMatcher/src/character/Player.java
+++ RoommateMatcher/src/character/Player.java
@@ -456,31 +456,11 @@ public class Player implements Hitboxable {
 	}
 
 	public void getShot(Player other) {
-		// if (opponent != null) {
-		// if (!justHit && health > opponent.getGun().getDamage()) {
-		// health -= opponent.getGun().getDamage();
-		// justHit = true;
-		// opponent.getUser().changeOffense(0.05);
-		// this.getUser().changeDefense(-0.05);
-		// } else if (health < opponent.getGun().getDamage()) {
-		// System.out.println("EEPS");
-		// health = 0;
-		// alive = false;
-		// } else {
-		// System.out.println("OPPS");
-		// while (opponent != null) {
-		// DrawingSurface.p1.setOpponent(DrawingSurface.p2);
-		// DrawingSurface.p2.setOpponent(DrawingSurface.p1);
-		// System.out.println("I cry");
-		// }
-		// }
-		// }
-
 		if (!justHit && health > other.getGun().getDamage()) {
 			health -= other.getGun().getDamage();
+			other.getUser().changeOffense(0.1);
 			justHit = true;
 		} else if (health <= other.getGun().getDamage()) {
-
 			health = 0;
 			alive = false;
 			DrawingSurface.gameOver = true;
--- RoommateMatcher/src/stages/Instructions.java
+++ RoommateMatcher/src/stages/Instructions.java
@@ -21,51 +21,12 @@ public class Instructions extends StageType {
 		p.pushStyle();
 		p.stroke(255);
 		p.fill(255);
-		Button menu = new Button((int) (p.width / 2.8), 0, p, "Menu");
-		// p.textSize(p.width / 50);
-		// p.text("Welcome to the Roommate Matcher!\n\n"
-		// + "In this game you can speed through the process of finding a roommate by
-		// playing a game! "
-		// + "Instead of a boring questionnaire where you craft responses to look like
-		// an angel, "
-		// + "let out your inner gamer and show your true qualities off! With every step
-		// of the game, "
-		// + "you fight with another character, and will be matched using our current
-		// databases to another character who fits your qualities!\n\n"
-		// + "Click the MENU button to leave the instructions screen!"
-		// + "Pick the arena you would like to fight in\n" + "Pick the user you would
-		// like to fight with\n"
-		// + "---------------------------------------------------\n" + "Controls:\n"
-		// + "Player 1 controls: Arrowkeys to move, down arrow to shoot and shift to use
-		// melee weapon\n"
-		// + "Player 2 controls: WAD to move, S to shoot and F to use melee weapon",
-		// p.width / 50,
+		p.textSize(p.height/30);
+		p.translate(p.width/2, 0);
+		p.textAlign(p.CENTER, p.CENTER);
+		Button menu = new Button((int) (-p.width/8), 0, p, "Menu");
 
-		// p.text("Welcome to the Roommate Matcher!\n\n"
-		// + "In this game you can speed through the process of finding a roommate by
-		// playing a game! + \n"
-		// + "Instead of a boring questionnaire where you craft responses to look like
-		// an angel, "
-		// + "let out your inner gamer and show your true qualities off! With every step
-		// of the game, "
-		// + "let out your inner gamer and show your true qualities off! With every
-		// step\n"
-		// + " of the game, you fight with another character, and will be matched using
-		// our current\n"
-		// + " databases to another character who fits your qualities!\n\n", (float)
-		// (p.height / 8.125),
-		// p.width, p.height);
-		//
-		// p.text("Click the MENU button to leave the instructions screen!" + "Pick the
-		// arena you would like to fight in\n"
-		// + "Pick the user you would like to fight with\n"
-		// + "---------------------------------------------------\n" + "Controls:\n"
-		// + "Player 1 controls: Arrowkeys to move, down arrow to shoot and shift to use
-		// melee weapon\n"
-		// + "Player 2 controls: WAD to move, S to shoot and F to use melee weapon",
-		// p.width / 50, 300);
-
-		float newLine = p.height / 23;
+		float newLine = p.height / 30;
 		float first = p.height / 20 + menu.getHeight();
 
 		p.text("Welcome to the Roommate Matcher!", 0, first);
@@ -74,22 +35,29 @@ public class Instructions extends StageType {
 		p.fill(0);
 		p.text("Player One: ", 0, first + 3 * newLine);
 		p.fill(255);
-		p.text("Arrow Keys     ->      Move Around", 0, first + 4 * newLine);
-		p.text("Period            ->      Shoot", 0, first + 5 * newLine);
-		p.text("Comma            ->      Toggle Gun", 0, first + 6 * newLine);
-		p.text("M                  ->      Toggle Blade", 0, first + 7 * newLine);
+		p.textAlign(p.LEFT, p.CENTER);
+		p.text("Arrow Keys    -    Move Around", 0, first + 4 * newLine);
+		p.text("Period    -    Shoot", 0, first + 5 * newLine);
+		p.text("Comma    -    Toggle Gun", 0, first + 6 * newLine);
+		p.text("M    -    Toggle Blade", 0, first + 7 * newLine);
 
+		p.textAlign(p.CENTER, p.CENTER);
 		p.fill(0);
 		p.text("Player Two: ", 0, first + 9 * newLine);
 		p.fill(255);
-		p.text("WASD              ->      Move Around", 0, first + 10 * newLine);
-		p.text("F                   ->      Shoot", 0, first + 11 * newLine);
-		p.text("Q                   ->     Toggle Gun", 0, first + 12 * newLine);
-		p.text("E                   ->     Toggle Blade", 0, first + 13 * newLine);
+		p.textAlign(p.LEFT, p.CENTER);
+		p.text("WASD    -    Move Around", 0, first + 10 * newLine);
+		p.text("F    -    Shoot", 0, first + 11 * newLine);
+		p.text("Q    -    Toggle Gun", 0, first + 12 * newLine);
+		p.text("E    -    Toggle Blade", 0, first + 13 * newLine);
 		
 		p.text("Gun Power Up increases fire rate", 0, first  + 16 * newLine);
 		p.text("Bullet Power Up increases bullet speed", 0, first  + 17 * newLine);
 		p.text("Health Power Up increases health", 0, first  + 18 * newLine);
+		
+		p.text("Rifle fires faster and does 2 damage", 0, first + 20*newLine);
+		p.text("Shotgun fires slower and does 10 damage", 0, first + 21*newLine);
+		p.text("Knife does 10 damage and Sword does 20 damage", 0, first + 22*newLine);
 
 		menu.draw();
 		p.popStyle();
